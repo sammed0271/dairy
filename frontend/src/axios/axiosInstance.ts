a7cf284cf04313efe8d5cf8e3aabb116d14bdf08
Sammed Mench <sammedmench11@gmail.com>
2026-05-03 16:35:58 +0530

    Update axiosInstance.ts

--- frontend/src/axios/axiosInstance.ts
+++ frontend/src/axios/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   // import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api"
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://w3058xp8-5000.inc1.devtunnels.ms/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
     "Cache-Control": "no-cache", // ✅ ADD THIS
